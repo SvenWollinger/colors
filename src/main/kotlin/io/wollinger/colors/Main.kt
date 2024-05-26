8cb488d9a2c12e54c437206d789bf50efa80bf8e
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2024-05-26 09:31:53 +0200

    setup

--- src/main/kotlin/io/wollinger/colors/Main.kt
+++ src/main/kotlin/io/wollinger/colors/Main.kt
@@ -1,5 +1,32 @@
 package io.wollinger.colors
 
+import kotlinx.browser.document
+import org.w3c.dom.HTMLDivElement
+
 fun main() {
-    console.log("Hello world!")
+    val colorsTable = id<HTMLDivElement>("colors")
+
+    document.onfullscreenchange = {
+        if(!document.fullscreen) {
+            println("Fullscreen off")
+            colorsTable.style.visibility = "visible"
+            document.body!!.style.backgroundColor = "white"
+            document.body!!.style.asDynamic().overflow = "auto"
+        }
+    }
+    Color.defaultColors.forEach { color ->
+        val div = (document.createElement("div") as HTMLDivElement).apply {
+            innerHTML = color.name
+            style.backgroundColor = color.cssString
+            classList.add("item", "text-border")
+        }
+        div.onclick = {
+            colorsTable.style.visibility = "hidden"
+            document.body!!.style.backgroundColor = color.cssString
+            document.body!!.style.asDynamic().overflow = "hidden"
+            document.documentElement!!.requestFullscreen()
+        }
+        colorsTable.append(div)
+    }
+    //console.log("Hello world!")
 }
